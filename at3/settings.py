2ed9b0a0bd15d111fbab84eebc35e783d6883166
Oak <OakE@na.pxl.int>
2017-02-03 17:27:55 -0500

    Heoroku

--- at3/settings.py
+++ at3/settings.py
@@ -128,3 +128,20 @@ STATIC_URL = '/static/'
 BOOTSTRAP3 = {
     'include_jquery' : True
     }
+
+#heroku settings
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+    }
+
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    ALLOWED_HOSTS = ['*']
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
