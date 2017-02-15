9838d2c0d0374c3c4983ed16e5f1df324cdda8a1
Oak <OakE@na.pxl.int>
2017-02-15 15:56:08 -0500

    early reporting

--- reports/views.py
+++ reports/views.py
@@ -2,11 +2,48 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from tracker.models import Tracker, Finance
 from django.core.urlresolvers import reverse
+from datetime import datetime, date
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'reports/home.html')
+    data = Tracker.objects.all()
+
+    open = Tracker.objects.filter(end_date__isnull=True)
+    acount = 0.0
+    asum = 0.0
+    cat_sum_dict = {}
+    cat_count_dict = {}
+
+    for t in open:
+        ed = date.today()
+        sd = t.start_date
+        day  = ed - sd
+        acount = acount + 1
+        asum = asum + day.days
+
+        if t.type in cat_sum_dict:
+            a = 1 + 1
+        else:
+            cat_sum_dict[t.type] = 0.0
+
+        if t.type in cat_count_dict:
+            b = 1 + 1
+        else:
+            cat_count_dict[t.type] = 0.0
+
+        cat_sum_dict[t.type] = cat_sum_dict[t.type] + day.days
+        cat_count_dict[t.type] = cat_count_dict[t.type] + 1
+
+    all_average = asum / acount
+
+    cat_avg = {}
+    for cat in cat_sum_dict.keys():
+        avg = cat_sum_dict[cat] / cat_count_dict[cat]
+        cat_avg[cat] = avg
+
+    context = {'data': data, 'all_average': all_average, 'cat_avg': cat_avg}
+    return render(request, 'reports/home.html', context)
 
 
 def at_csv_dump(request):
