1ecfbc23f6b28862cfd81f3deaa3df8370fb4a86
Oak <OakE@na.pxl.int>
2017-02-16 13:27:41 -0500

    basic reporting page updated

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Historical(models.Model):
+    date = models.DateField(auto_now_add=True)
+    key = models.CharField(max_length=140)
+    value = models.DecimalField(max_digits=19, decimal_places = 5)
+    def  __str__(self):
+        return str(self.key) + str(self.date)
--- reports/views.py
+++ reports/views.py
@@ -7,36 +7,38 @@ from datetime import datetime, date
 # Create your views here.
 
 def home(request):
+    #grab all data
     data = Tracker.objects.all()
-
+    #grab data filed to open items
     open = Tracker.objects.filter(end_date__isnull=True)
     acount = 0.0
     asum = 0.0
     cat_sum_dict = {}
     cat_count_dict = {}
-
+    #run through open items and calculate averages
     for t in open:
         ed = date.today()
         sd = t.start_date
         day  = ed - sd
         acount = acount + 1
         asum = asum + day.days
-
+        #make sure the key exists in the dictionary
         if t.type in cat_sum_dict:
             a = 1 + 1
         else:
             cat_sum_dict[t.type] = 0.0
-
+        #make sure the key exists in the dictionary
         if t.type in cat_count_dict:
             b = 1 + 1
         else:
             cat_count_dict[t.type] = 0.0
-
+        #write the intances data to the dictionary
         cat_sum_dict[t.type] = cat_sum_dict[t.type] + day.days
         cat_count_dict[t.type] = cat_count_dict[t.type] + 1
-
+    #cacluate all average
     all_average = asum / acount
 
+    #calcualte avg by category
     cat_avg = {}
     for cat in cat_sum_dict.keys():
         avg = cat_sum_dict[cat] / cat_count_dict[cat]
