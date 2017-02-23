361ec89c56306841330b937f9b8a8f0c3b8cfb4a
Oak <OakE@na.pxl.int>
2017-02-23 09:05:04 -0500

    fleshed out models in contract app

--- contract/models.py
+++ contract/models.py
@@ -7,22 +7,40 @@ class Contact(models.Model):
     last = models.CharField(max_length=140)
     email = models.EmailField(max_length=254, blank=True)
     number = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField()
+    viewed = models.DateTimeField()
+
     def __str__(self):
         return self.first + ' ' + self.last
 
 class Supplier(models.Model):
     name = models.CharField(max_length=140)
     main_contact = models.ForeignKey(Contact)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField()
+    viewed = models.DateTimeField()
     def __str__(self):
         return self.name
 
 class Contract(models.Model):
     name = models.CharField(max_length=140)
+    eff_date = models.DateField()
+    end_date = models.DateField()
+    perp = models.NullBooleanField()
+    term_note = models.IntegerField()
+    auto_renew = models.BooleanField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField()
+    viewed = models.DateTimeField()
     def __str__(self):
         return self.name
 
 class Note(models.Model):
     note = models.TextField()
     supplier = models.ForeignKey(Supplier)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField()
+    viewed = models.DateTimeField()
     def __str__(self):
         return self.note
