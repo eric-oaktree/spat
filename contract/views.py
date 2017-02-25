08aedeb592b3e73f67b5f3b34d546b44649082f6
Oak <erkoak@gmail.com>
2017-02-25 18:52:23 -0500

    tweak

--- contract/views.py
+++ contract/views.py
@@ -5,17 +5,6 @@ from .models import Contract
 
 def recent_con(request):
     cons = Contract.objects.all().order_by('-viewed')
-#    i = 0
-#    con_dict = {}
-#    b = []
     con = cons[0:19]
-#    while i <= 20:
-#        con = cons[i]
-#        name = con.name
-#        eff = con.eff_date
-#        sup = con.supplier
-#        a = (name, eff, sup)
-#
-#        i = i++
     context = {'con': con}
     return render(request, 'contract/list.html', context)
